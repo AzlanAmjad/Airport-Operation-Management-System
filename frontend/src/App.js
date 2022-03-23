d9319b821558c55baf6be4ca62a9a2a9bf4f4641
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-23 15:34:21 -0600

    reorganized project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
-import Home from './Home';
-import NavBar from './NavBar';
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
-
     </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,10 @@
 import { useState, useEffect } from 'react';
 
-
 const Home = () => {
-
     return (
-
         <div>
             <h2>Home</h2>
         </div>
-
-
-
     );
 }
 
