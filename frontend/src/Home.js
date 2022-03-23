84eb7362f068d847c2e78218d04532b96bcaaf07
Tr1d3nt <saudagha2@gmail.com>
2022-03-23 14:25:23 -0600

    setup

---
+++ frontend/src/Home.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+
+const Home = () => {
+
+    return (
+
+        <div>
+            <h2>Home</h2>
+        </div>
+
+
+
+    );
+}
+
+export default Home;
