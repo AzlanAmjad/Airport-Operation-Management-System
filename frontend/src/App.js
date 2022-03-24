9786ca0df44680bdcf56bb4143e53e8a992b488d
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-24 15:40:26 -0600

    added to material UI grid to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,34 @@ import About from "./components/About";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
+// material UI
+import Grid from "@mui/material/Grid";
+
 function App() {
   return (
-    <div>
-      <div>Hello World</div>
+    <Grid
+      container
+      direction="column"
+      justify-content="space-between"
+      align-items="center"
+    >
+      <Grid item>
+        <div>Header</div>
+      </Grid>
 
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-    </div>
+      <Grid item>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Grid>
+
+      <Grid item>
+        <div>Footer</div>
+      </Grid>
+    </Grid>
   );
 }
 
