7c45630bb5071f359e2430667cb3272cceba117a
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-25 05:11:40 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,10 +60,10 @@ function App() {
           <Grid item>
             <Routes>
               <Route path="/" element={<Home />}></Route>
-              <Route path="/flight-details/:airline/:flight" element={<Flight />}></Route>
               <Route path="/about" element={<About />}></Route>
               <Route path="/login" element={<Login />}></Route>
               <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/flight-details/:airline/:flight" element={<Flight />}></Route>
             </Routes>
           </Grid>
 
