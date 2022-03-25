0d58e14530299dad65fb9f8ba8d6a5f267d51b9c
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-24 19:25:00 -0600

    added material UI theming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,31 +9,45 @@ import Signup from "./pages/Signup";
 // material UI
 import Grid from "@mui/material/Grid";
 
+// material UI theming
+import { red } from "@mui/material/colors";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[400],
+    },
+  },
+});
+
 function App() {
   return (
-    <Grid
-      container
-      direction="column"
-      justify-content="space-between"
-      align-items="center"
-    >
-      <Grid item>
-        <div>Header</div>
-      </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        direction="column"
+        justify-content="space-between"
+        align-items="center"
+      >
+        <Grid item>
+          <div>Header</div>
+        </Grid>
 
-      <Grid item>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-        </Routes>
-      </Grid>
+        <Grid item>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+          </Routes>
+        </Grid>
 
-      <Grid item>
-        <div>Footer</div>
+        <Grid item>
+          <div>Footer</div>
+        </Grid>
       </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
 
