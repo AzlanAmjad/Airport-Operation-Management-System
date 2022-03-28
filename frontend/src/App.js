74e832276dc5cd8cbc9ef77eba6e4452fdfd105a
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-28 06:00:54 -0600

    formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -96,7 +96,7 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/airlinedash/*" element={<AirlineDashboard />} />
+              <Route path="/airline-dashboard/*" element={<AirlineDashboard />} />
             </Routes>
           </Grid>
 
