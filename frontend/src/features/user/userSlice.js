0a47fa13819a0d6717afc7c554dae1411014f6bb
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-28 18:52:57 -0600

    created userSlice template

---
+++ frontend/src/features/user/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: {}
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        
+    }
+})
