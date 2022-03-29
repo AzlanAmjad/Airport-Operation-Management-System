692f4a4710f9b56ebb5ba164ba82456b79f94b73
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-28 18:22:49 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
