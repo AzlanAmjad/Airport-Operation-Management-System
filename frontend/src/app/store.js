dbe1baaa2b8146513da0ccdde99f6702836308ed
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-28 19:23:57 -0600

    defined simple login reducer for userSlice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -1,16 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: {}
-}
+  value: {},
+};
 
+// user slice of state
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-
+  name: "user",
+  initialState,
+  // define reducers with associated actions
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
     },
-    extraReducers: (builder) => {
-        
-    }
-})
+  },
+  extraReducers: (builder) => {},
+});
+
+// actions
+export const { login } = userSlice.actions;
+
+// selector
+export const selectUser = (state) => state.userSlice.value;
+
+// reducer
+export default userSlice.reducer;
