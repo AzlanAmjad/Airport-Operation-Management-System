e63a6bf1e6c866510c981c1cc83f866912fa083d
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-03-30 18:48:20 -0600

    Update Flight.js

--- frontend/src/pages/Flight.js
+++ frontend/src/pages/Flight.js
@@ -1,8 +1,12 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Paper, IconButton } from "@mui/material";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Flight = () => {
+  const navigate = useNavigate();
+
   const { airline, flight_num } = useParams();
   const airline_name = airline.replace(/\++/g, " ");
 
@@ -22,6 +26,26 @@ const Flight = () => {
     city: "Toronto",
     country: "Canada",
   });
+
+  // tickets aggregated based on their type for this flight
+  const [fares, setFare] = useState([
+    {
+      cabin: "First",
+      tickets: 150,
+    },
+    {
+      cabin: "Business",
+      tickets: 200,
+    },
+    {
+      cabin: "Premium Economy",
+      tickets: 300,
+    },
+    {
+      cabin: "Economy",
+      tickets: 600,
+    },
+  ]);
 
   return (
     <Grid
@@ -48,14 +72,68 @@ const Flight = () => {
         </Grid>
         <Grid item>
           <Typography>
-            {flight.airline_name} - {flight.dep_time.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}
+            {flight.airline_name} -{" "}
+            {flight.dep_time.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}
           </Typography>
         </Grid>
       </Grid>
       <Grid item>
         <Typography>
-          {flight.dep_time.match(/[0-9]{2}:[0-9]{2}/)} - {flight.arrival_time.match(/[0-9]{2}:[0-9]{2}/)}
+          {flight.dep_time.match(/[0-9]{2}:[0-9]{2}/)} -{" "}
+          {flight.arrival_time.match(/[0-9]{2}:[0-9]{2}/)}
         </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItem="center"
+        justifyContent="space-evenly"
+        rowSpacing={3}
+        my="50px"
+      >
+        <Grid item>
+          <Typography variant="h6">
+            Select Fare to {destination.city}
+          </Typography>
+        </Grid>
+
+        {fares.map((fare) => {
+          return (
+            <Grid item key={`${fare.cabin}`} sx={{ width: "inherit" }}>
+              <Paper elevation={12} sx={{ padding: "30px" }}>
+                <Grid
+                  container
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  wrap="nowrap"
+                >
+                  <Grid
+                    item
+                    container
+                    direction="column"
+                    alignItems="flex-start"
+                    justifyContent="space-evenly"
+                    rowSpacing={1}
+                  >
+                    <Grid item>
+                      <Typography fontWeight="bold">{fare.cabin}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>{fare.tickets} Tickets Left</Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <IconButton color="inherit" onClick={() => {}}>
+                      <ChevronRightIcon fontSize="large" />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          );
+        })}
       </Grid>
     </Grid>
   );
