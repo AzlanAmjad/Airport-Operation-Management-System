9d017151fa9216c034bbc7e3d12e4e9a331ca01d
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-04 17:32:03 -0600

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+# view for all endpoints
+def getRoutes(request):
+    routes = [
+        '/token',
+        '/token/refresh'
+    ]
+    return JsonResponse(routes, safe=False)
