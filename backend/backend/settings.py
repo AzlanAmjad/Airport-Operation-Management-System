dbc64e906cd02bba3be04c084843d1b40ab2d31f
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-05 00:32:42 -0600

    database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,8 +99,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airportdb',
+        'USER': 'aiport-system-api',
+        'PASSWORD': 'cpsc471',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'TEST': {},
     }
 }
 
