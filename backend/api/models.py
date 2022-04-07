5638950e1d1d355e6b0c1341472afe0903c9a7e8
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 04:01:56 -0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,89 +1,131 @@
-from asyncio.windows_events import NULL
-from curses.ascii import NUL
 from django.db import models
 
 # Create your models here.
 
-# Hotel model
-class Hotel(models.Model):
-    name = NULL
+# Passenger model
+class Passenger(models.Model):
+    pass
 
     class Meta:
-        db_table = 'hotel'
+        db_table = 'passenger'
 
-# Transaction model
-class Transaction(models.Model):
-    name = NULL
 
-    class Meta:
-        db_table = 'transaction'
-
-# Stay model
-class Stay(models.Model):
-    name = NULL
+# Airport Admin model
+class AirportAdmin(models.Model):
+    pass
 
     class Meta:
-        db_table = 'stay'
+        db_table = 'airport_admin'
+
+
+# Airline Admin model
+class AirlineAdmin(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'airline_admin'
+
 
 # Company model
 class Company(models.Model):
-    name = NULL
+    name = models.CharField(max_length=255, primary_key=True)
+    a_email = models.ForeignKey(AirportAdmin, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'company'
 
+
+# Hotel model
+class Hotel(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'hotel'
+
+
+# Transaction model
+class Transaction(models.Model):
+    transac_id = models.PositiveBigIntegerField(primary_key=True)
+    p_email = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'transaction'
+
+
+# Stay model
+class Stay(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'stay'
+
+
 # Airport Complaint model
 class AirportComplaint(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'airport_complaint'
 
+
 # Airline Complaint model
 class AirlineComplaint(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'airline_complaint'
 
+
 # Airline model
 class Airline(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'airline'
 
+
 # Airplane model
 class Airplane(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'airplane'
 
+
 # Flight model
 class Flight(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'flight'
 
+
 # Destination model
 class Destination(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'destination'
 
+
 # Fare model
 class Fare(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'fare'
 
+
 # Ticket model
 class Ticket(models.Model):
-    name = NULL
+    pass
 
     class Meta:
         db_table = 'ticket'
+
