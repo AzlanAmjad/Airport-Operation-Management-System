e2507a3dd811fb6490e64f88ed449a3ca91eabfd
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 03:15:55 -0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,46 +8,82 @@ from django.db import models
 class Hotel(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'hotel'
+
 # Transaction model
 class Transaction(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'transaction'
 
 # Stay model
 class Stay(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'stay'
+
 # Company model
 class Company(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'company'
 
 # Airport Complaint model
 class AirportComplaint(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'airport_complaint'
+
 # Airline Complaint model
 class AirlineComplaint(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'airline_complaint'
 
 # Airline model
 class Airline(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'airline'
+
 # Airplane model
 class Airplane(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'airplane'
 
 # Flight model
 class Flight(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'flight'
+
 # Destination model
 class Destination(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'destination'
 
 # Fare model
 class Fare(models.Model):
     name = NULL
 
+    class Meta:
+        db_table = 'fare'
+
 # Ticket model
 class Ticket(models.Model):
     name = NULL
+
+    class Meta:
+        db_table = 'ticket'
