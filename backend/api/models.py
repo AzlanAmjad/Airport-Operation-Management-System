878a11e212bfcd552ae2a81fa5eded0356e8440b
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 15:22:36 -0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,3 @@
-from ast import Pass
-from doctest import debug_script
 from django.db import models
 
 # Create your models here.
@@ -120,25 +118,42 @@ class Airplane(models.Model):
         db_table = 'airplane'
 
 
-# Flight model
-class Flight(models.Model):
-    pass
-
-    class Meta:
-        db_table = 'flight'
-
-
 # Destination model
 class Destination(models.Model):
-    pass
+    airport_code = models.CharField(max_length=3, primary_key=True)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'destination'
 
 
+# Flight model
+class Flight(models.Model):
+    # auto generated primary key since django does not support composite primary keys
+    # flight_num, airline_name will be unique together (UniqueConstraint)
+    flight_num = models.PositiveIntegerField()
+    airline_name = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    dep_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    dest_code = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    plane_id = models.ForeignKey(Airplane, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'flight'
+        constraints = [
+            models.UniqueConstraint(fields=['flight_num', 'airline_name'], name='unique_flight'),
+        ]
+
+
 # Fare model
 class Fare(models.Model):
-    pass
+    fare_id = models.PositiveIntegerField(primary_key=True)
+    price = models.PositiveIntegerField()
+    cabin = models.CharField(max_length=255)
+    airline_name = models.ForeignKey(Flight, to_field='airline_name', on_delete=models.CASCADE)
+    flight_num = models.ForeignKey(Flight, to_field='flight_num', on_delete=models.CASCADE)
+    tickets = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'fare'
@@ -146,7 +161,10 @@ class Fare(models.Model):
 
 # Ticket model
 class Ticket(models.Model):
-    pass
+    ticket_id = models.PositiveIntegerField(primary_key=True)
+    seat_pos = models.CharField(max_length=255)
+    p_email = models.ForeignKey(Passenger, on_delete=models.SET_NULL, null=True)
+    fare_id = models.ForeignKey(Fare, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'ticket'
