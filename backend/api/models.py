1c207f2a5aa871be5c3c185e1a79dd13f1b76bd6
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 18:03:02 -0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 from django.contrib.auth import get_user_model
-User = get_user_model()
+user = get_user_model()
 
 # Create your models here.
 
