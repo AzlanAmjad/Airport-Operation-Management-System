10b67e213b2aecab548b2cbe30c91004be60a3ea
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-08 04:15:17 -0600

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,4 +12,4 @@ urlpatterns = [
 
     path('__debug__/', include('debug_toolbar.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
