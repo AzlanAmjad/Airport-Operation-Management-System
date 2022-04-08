52912b0cfdbd9ea1d1b3b4aef1e875bca5cbfb0b
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 19:24:14 -0600

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(User, CustomUserAdmin)
