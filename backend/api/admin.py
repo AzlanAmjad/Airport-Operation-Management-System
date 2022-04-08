1f01fbf13111c0dd7e40763679ed317eecce6ca0
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 19:35:48 -0600

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .models import User
+from . import models
 
 class CustomUserAdmin(UserAdmin):
     ordering = ('email',)
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(models.User, CustomUserAdmin)
+admin.site.register(models.Passenger)
+admin.site.register(models.AirportAdmin)
+admin.site.register(models.AirlineAdmin)
+admin.site.register(models.Company)
+admin.site.register(models.Hotel)
+admin.site.register(models.Transaction)
+admin.site.register(models.Stay)
+admin.site.register(models.AirportComplaint)
+admin.site.register(models.Airline)
+admin.site.register(models.AirlineComplaint)
+admin.site.register(models.Airplane)
+admin.site.register(models.Destination)
+admin.site.register(models.Flight)
+admin.site.register(models.Fare)
+admin.site.register(models.Ticket)
+
