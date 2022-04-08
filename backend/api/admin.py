b6f384a8c7cf439b95c8319a22c69b03f6c98cef
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-07 19:38:44 -0600

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,6 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 class CustomUserAdmin(UserAdmin):
+    model = models.User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
     ordering = ('email',)
 
 admin.site.register(models.User, CustomUserAdmin)
