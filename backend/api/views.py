b6d27dc18c878b33b6e5c9ee995a7a06f2b1ac9a
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-09 02:41:11 -0600

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .models import User, Passenger, AirportAdmin, AirlineAdmin, Company, Hotel, Transaction, Stay, AirportComplaint, Airline, AirlineComplaint, Airplane, Destination, Flight, Fare, Ticket
+from .serializers import UserSerializer, PassengerSerializer, AirportAdminSerializer, AirlineAdminSerializer, CompanySerializer, HotelSerializer, TransactionSerializer, StaySerializer, AirportComplaintSerializer, AirlineSerializer, AirlineComplaintSerializer, AirplaneSerializer, DestinationSerializer, FlightSerializer, FareSerializer, TicketSerializer 
 
 # Create your views here.
 
