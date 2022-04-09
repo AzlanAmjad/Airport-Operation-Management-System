14647ff3d003fb0928b9c16eda10283833e438f4
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-09 03:15:58 -0600

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,10 +7,3 @@ from .serializers import UserSerializer, PassengerSerializer, AirportAdminSerial
 
 # Create your views here.
 
-# view for all endpoints
-def getRoutes(request):
-    routes = [
-        '/token',
-        '/token/refresh'
-    ]
-    return JsonResponse(routes, safe=False)
