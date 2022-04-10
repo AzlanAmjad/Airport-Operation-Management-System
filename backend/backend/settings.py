bcd607e29577582121edea66a5dd16734666e0a9
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-10 12:03:55 -0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,9 +74,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ),
+    ],
 }
 
 SIMPLE_JWT = {
