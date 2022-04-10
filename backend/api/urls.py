bead59bdc5a8d18b04f19826f373a05131c8aca8
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-10 01:55:56 -0600

    so many endpoints :weary:

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,44 +4,73 @@ from api.models import AirportComplaint
 from . import views
 
 urlpatterns = [
-    # get all destinations
-    path('destinations/', views.Destinations.as_view()),
-    # get searched flights
-    path('flights/<slug:destination>/<slug:departure>/', views.SearchFlights.as_view()),
-    # get flight, # put flight, # delete flight
-    path('flight/<int:flight>/', views.Flight.as_view()),
-    # get destination
-    path('destination/<slug:destination>/', views.Destination.as_view()),
-    # get flight fares
-    path('fares/<int:flight>/', views.FlightFares.as_view()),
-    # get all companies
-    path('companies/', views.AllCompanies.as_view()),
-    # get company hotels
-    path('hotels/<int:company>/', views.CompanyHotels.as_view()),
-    # get hotel stays
-    path('stays/<int:hotel>/', views.HotelStays.as_view()),
-    # get any fare ticket
-    path('fare/ticket/<int:fare>/', views.FareTicket.as_view()),
-    # put ticket
-    path('ticket/<int:ticket>/', views.Ticket.as_view()),
-    # post transaction
-    path('transaction/', views.Transaction.as_view()),
-    # put stay
-    path('stay/<int:stay>/', views.Stay.as_view()),
-    # get airline flights
-    path('flights/<int:airline>/', views.AirlineFlights.as_view()),
+    # FLIGHT
     # post flight
     path('flight/', views.Flight.as_view()),
+    # get flight, # put flight, # delete flight
+    path('flight/<int:flight>/', views.Flight.as_view()),
+
+    # DESTINATION
+    # get destination
+    path('destination/<slug:destination>/', views.Destination.as_view()),
+    # get all destinations
+    path('destinations/', views.Destinations.as_view()),
+
+    # TICKET
+    # put ticket
+    path('ticket/<int:ticket>/', views.Ticket.as_view()),
+
+    # TRANSACTION
+    # post transaction
+    path('transaction/', views.Transaction.as_view()),
+
+    # STAY
+    # post stay
+    path('stay/', views.Stay.as_view()),
+    # put stay
+    path('stay/<int:stay>/', views.Stay.as_view()),
+
+    # AIRLINE COMPLAINT
     # post airline complaint
     path('airline-complaint/', views.AirlineComplaint.as_view()),
     # put airline complaint, delete airline complaint
     path('airline-complaint/<int:complaint>/', views.AirlineComplaint.as_view()),
     # get airline complaints
     path('airline-complaints/<int:airline>/', views.AirlineComplaints.as_view()),
+
+    # AIRPORT COMPLAINT
     # post airport complaint
     path('airport-complaint/', views.AirportComplaint.as_view()),
     # put airport complaint, delete airport complaint
     path('airport-complaint/<int:complaint>/', views.AirportComplaint.as_view()),
     # get airport complaints
     path('airport-complaints/', views.AirportComplaints.as_view()),
+
+    # COMPANY
+    # post company
+    path('company/', views.Company.as_view()),
+    # put company, delete company
+    path('company/<int:company>', views.Company.as_view()),
+    # get companies
+    path('companies/', views.Companies.as_view()),
+
+    # HOTEL
+    # post hotel
+    path('hotel/', views.Hotel.as_view()),
+    # put hotel
+    path('hotel/<int:hotel>', views.Hotel.as_view()),
+
+    # MORE COMPLICATED END-POINTS
+    # get searched flights
+    path('flights/<slug:destination>/<slug:departure>/', views.SearchFlights.as_view()),
+    # get flight fares
+    path('fares/<int:flight>/', views.FlightFares.as_view()),
+    # get company hotels
+    path('hotels/<int:company>/', views.CompanyHotels.as_view()),
+    # get hotel stays
+    path('stays/<int:hotel>/', views.HotelStays.as_view()),
+    # get any fare ticket
+    path('fare/ticket/<int:fare>/', views.FareTicket.as_view()),
+    # get airline flights
+    path('flights/<int:airline>/', views.AirlineFlights.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,26 +6,11 @@ from rest_framework.response import Response
 from . import models
 from . import serializers
 
-
-class Destinations(APIView):
-    def get(self, request, format=None):
-        destinations = models.Destination.objects.all()
-        serializer = serializers.DestinationSerializer(destinations, many=True)
-        return Response(serializer.data)
-
-
-class SearchFlights(APIView):
-    def get(self, request, destination, departure, format=None):
-        date = datetime.datetime.strptime(departure, '%Y-%m-%d').date()
-        flights = models.Flight.objects.filter(destination=destination).filter(dep_time__date=date)
-        serializer = serializers.FlightSerializer(flights, many=True)
-        return Response(serializer.data)
-
-
+# FLIGHT
 class Flight(APIView):
     def get(self, request, flight, format=None):
-        flight = models.Flight.objects.get(pk=flight)
-        serializer = serializers.FlightSerializer(flight)
+        _flight = models.Flight.objects.get(pk=flight)
+        serializer = serializers.FlightSerializer(_flight)
         return Response(serializer.data)
 
     def post(self, request, format=None):
@@ -36,71 +21,45 @@ class Flight(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, flight, format=None):
-        flight = models.Flight.objects.get(pk=flight)
-        serializer = serializers.FlightSerializer(flight, data=request.data)
+        _flight = models.Flight.objects.get(pk=flight)
+        serializer = serializers.FlightSerializer(_flight, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, flight, format=None):
-        flight = models.Flight.objects.get(pk=flight)
-        flight.delete()
+        _flight = models.Flight.objects.get(pk=flight)
+        _flight.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# DESTINATION
 class Destination(APIView):
     def get(self, request, destination, format=None):
         oneDestination = models.Destination.objects.get(pk=destination)
         serializer = serializers.DestinationSerializer(oneDestination)
         return Response(serializer.data)
 
-
-class FlightFares(APIView):
-    def get(self, request, flight, format=None):
-        fares = models.Fare.objects.filter(flight=flight)
-        serializer = serializers.FareSerializer(fares, many=True)
-        return Response(serializer.data)
-
-
-class AllCompanies(APIView):
+class Destinations(APIView):
     def get(self, request, format=None):
-        companies = models.Company.objects.all()
-        serializer = serializers.CompanySerializer(companies, many=True)
+        destinations = models.Destination.objects.all()
+        serializer = serializers.DestinationSerializer(destinations, many=True)
         return Response(serializer.data)
 
 
-class CompanyHotels(APIView):
-    def get(self, request, company, format=None):
-        hotels = models.Hotel.objects.filter(company=company)
-        serializer = serializers.HotelSerializer(hotels, many=True)
-        return Response(serializer.data)
-
-
-class HotelStays(APIView):
-    def get(self, request, hotel, format=None):
-        stays = models.Stay.objects.filter(hotel=hotel)
-        serializer = serializers.StaySerializer(stays, many=True)
-        return Response(serializer.data)
-
-
-class FareTicket(APIView):
-    def get(self, request, fare, format=None):
-        ticket = models.Ticket.objects.filter(fare=fare).filter(passenger=None).first()
-        serializer = serializers.TicketSerializer(ticket)
-        return Response(serializer.data)
-
-
+# TICKET
 class Ticket(APIView):
     def put(self, request, ticket, format=None):
-        ticket = models.Ticket.objects.get(pk=ticket)
-        serializer = serializers.TicketSerializer(ticket, data=request.data)
+        _ticket = models.Ticket.objects.get(pk=ticket)
+        serializer = serializers.TicketSerializer(_ticket, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# TRANSACTION
 class Transaction(APIView):
     def post(self, request, format=None):
         serializer = serializers.TicketSerializer(data=request.data)
@@ -110,23 +69,25 @@ class Transaction(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# STAY
 class Stay(APIView):
+    def post(self, request, format=None):
+        serializer = serializers.StaySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def put(self, request, stay, format=None):
-        stay = models.Stay.objects.get(pk=stay)
-        serializer = serializers.StaySerializer(stay, data=request.data)
+        _stay = models.Stay.objects.get(pk=stay)
+        serializer = serializers.StaySerializer(_stay, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AirlineFlights(APIView):
-    def get(self, request, airline, format=None):
-        flights = models.Flight.objects.filter(airline=airline)
-        serializer = serializers.FlightSerializer(flights, many=True)
-        return Response(serializer.data)
-
-
+# AIRLINE COMPLAINT
 class AirlineComplaint(APIView):
     def post(self, request, format=None):
         serializer = serializers.AirlineComplaintSerializer(data=request.data)
@@ -136,18 +97,17 @@ class AirlineComplaint(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, complaint, format=None):
-        complaint = models.AirlineComplaint.objects.get(pk=complaint)
-        serializer = serializers.AirlineComplaintSerializer(complaint, data=request.data)
+        _complaint = models.AirlineComplaint.objects.get(pk=complaint)
+        serializer = serializers.AirlineComplaintSerializer(_complaint, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, complaint, format=None):
-        complaint = models.AirlineComplaint.objects.get(pk=complaint)
-        complaint.delete()
+        _complaint = models.AirlineComplaint.objects.get(pk=complaint)
+        _complaint.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class AirlineComplaints(APIView):
     def get(self, request, airline, format=None):
@@ -156,6 +116,7 @@ class AirlineComplaints(APIView):
         return Response(serializer.data)
 
 
+# AIRPORT COMPLAINT
 class AirportComplaint(APIView):
     def post(self, request, format=None):
         serializer = serializers.AirportComplaintSerializer(data=request.data)
@@ -165,21 +126,106 @@ class AirportComplaint(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, complaint, format=None):
-        complaint = models.AirportComplaint.objects.get(pk=complaint)
-        serializer = serializers.AirportComplaintSerializer(complaint, data=request.data)
+        _complaint = models.AirportComplaint.objects.get(pk=complaint)
+        serializer = serializers.AirportComplaintSerializer(_complaint, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, complaint, format=None):
-        complaint = models.AirportComplaint.objects.get(pk=complaint)
-        complaint.delete()
+        _complaint = models.AirportComplaint.objects.get(pk=complaint)
+        _complaint.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class AirportComplaints(APIView):
     def get(self, request, format=None):
         complaints = models.AirportComplaint.objects.all()
         serializer = serializers.AirportComplaintSerializer(complaints, many=True)
+        return Response(serializer.data)
+
+
+# COMPANY
+class Company(APIView):
+    def post(self, request, format=None):
+        serializer = serializers.CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, company, format=None):
+        _company = models.Company.objects.get(pk=company)
+        serializer = serializers.CompanySerializer(_company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, company, format=None):
+        _company = models.Company.objects.get(pk=company)
+        _company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class Companies(APIView):
+    def get(self, request, format=None):
+        companies = models.Company.objects.all()
+        serializer = serializers.CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+
+# HOTEL
+class Hotel(APIView):
+    def post(self, request, format=None):
+        serializer = serializers.HotelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, hotel, format=None):
+        _hotel = models.Hotel.objects.get(pk=hotel)
+        serializer = serializers.HotelSerializer(_hotel, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# MORE COMPLICATED VIEWS
+class SearchFlights(APIView):
+    def get(self, request, destination, departure, format=None):
+        date = datetime.datetime.strptime(departure, '%Y-%m-%d').date()
+        flights = models.Flight.objects.filter(destination=destination).filter(dep_time__date=date)
+        serializer = serializers.FlightSerializer(flights, many=True)
+        return Response(serializer.data)
+
+class FlightFares(APIView):
+    def get(self, request, flight, format=None):
+        fares = models.Fare.objects.filter(flight=flight)
+        serializer = serializers.FareSerializer(fares, many=True)
+        return Response(serializer.data)
+
+class CompanyHotels(APIView):
+    def get(self, request, company, format=None):
+        hotels = models.Hotel.objects.filter(company=company)
+        serializer = serializers.HotelSerializer(hotels, many=True)
+        return Response(serializer.data)
+
+class HotelStays(APIView):
+    def get(self, request, hotel, format=None):
+        stays = models.Stay.objects.filter(hotel=hotel)
+        serializer = serializers.StaySerializer(stays, many=True)
+        return Response(serializer.data)
+
+class FareTicket(APIView):
+    def get(self, request, fare, format=None):
+        ticket = models.Ticket.objects.filter(fare=fare).filter(passenger=None).first()
+        serializer = serializers.TicketSerializer(ticket)
+        return Response(serializer.data)
+
+class AirlineFlights(APIView):
+    def get(self, request, airline, format=None):
+        flights = models.Flight.objects.filter(airline=airline)
+        serializer = serializers.FlightSerializer(flights, many=True)
         return Response(serializer.data)
