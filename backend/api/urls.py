dbdd5ae44d39fd1e7d177d2b0747dc5266860371
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-09 18:17:14 -0600

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('flights/<slug:destination>/<slug:departure>/', views.SearchFlights.as_view()),
     path('flight/<int:flight>/', views.OneFlight.as_view()),
     path('destination/<slug:airport_code>/', views.OneDestination.as_view()),
-    path('fares/<int:flight>', views.FlightFares.as_view())
+    path('fares/<int:flight>/', views.FlightFares.as_view())
 ]
