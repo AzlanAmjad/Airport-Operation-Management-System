4866b23e906eeb7e6e30f35707fff0022f3ca281
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-11 01:06:17 -0600

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,140 +1,125 @@
-import { Button } from "@mui/material";
-import { Typography } from "@mui/material";
-import { Grid } from "@mui/material";
-import { Box } from "@mui/material";
-import { Avatar } from "@mui/material";
-import { TextField } from "@mui/material";
-import { Link } from "@mui/material";
-import { Paper } from "@mui/material";
-import { orange } from "@mui/material/colors";
-import { styled } from "@mui/system";
-
-
+import { Button, Typography, Grid, TextField, Paper } from "@mui/material";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  
-  const stylePaper = {
-    marginBottom: 5,
-    padding: 15,
-    height: "80vh",
-    maxWidth: 450,
-    margin: "60px",
+  const hStyle = {
+    svg: "white",
+    input: "white",
+    label: "white",
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderColor: "white",
+    },
+    "&:hover .MuiOutlinedInput-notchedOutline": {
+      borderColor: "red",
+    },
+    "&.MuiOutlinedInput-notchedOutline.Mui-focused": {
+      borderColor: "red",
+    },
+    "& .MuiButtonBase-root.MuiAutocomplete-clearIndicator": {
+      color: "red",
+    },
+    "& .MuiButtonBase-root.MuiAutocomplete-popupIndicator": {
+      color: "white",
+    },
+    "& input[type=number]": {
+      "-moz-appearance": "textfield",
+    },
+    "& input[type=number]::-webkit-outer-spin-button": {
+      "-webkit-appearance": "none",
+      margin: 0,
+    },
+    "& input[type=number]::-webkit-inner-spin-button": {
+      "-webkit-appearance": "none",
+      margin: 0,
+    },
   };
-  const hStyle = { "& .MuiInputBase-root": {
-    color: "black"
-  },
-        backgroundColor : "whitesmoke",
 
-        "& label.Mui-focused":{
-          color: "black"
-        }, 
-        "& .MuiOutlinedInput-root": {
-          "&.Mui-focused fieldset": {
-            borderColor: "black"
-          }
-        }
-};
-
-const btnstyle = { margin: "20px 0", height: "5vh" };
-return (
-  <Grid>
-    <Paper elevation={12} style={stylePaper}>
-      <Grid 
-      container
-      direction= "column" 
-      alignItems="center"
-      >
-        <Avatar src="/Signup.png" />
-        <h2 >Sign Up</h2>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs = {6}>
-          <TextField
-          sx={{...hStyle}}
-          label="First Name"
-          placeholder="Fname"
-          fullWidth
-          required
-          />
+  return (
+    <Grid item container py="20px" width="500px">
+      <Paper elevation={12}>
+        <Grid item container direction="column" rowSpacing={4} padding="35px">
+          <Grid item container direction="column" alignItems="center">
+            <Grid item>
+              <PersonAddIcon fontSize="large" />
+            </Grid>
+            <Grid item>
+              <Typography variant="h3">Sign Up</Typography>
+            </Grid>
+          </Grid>
+          <Grid item container direction="row" spacing={3}>
+            <Grid item xs={6}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="First Name"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="Last Name"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="SSN"
+                type="number"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="Address"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="Email"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                sx={{ ...hStyle }}
+                placeholder="Password"
+                type="password"
+                fullWidth
+                required
+              />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" sx={{ minWidth: "200px" }}>
+              Create Account
+            </Button>
+          </Grid>
+          <Grid item container direction="column" alignItems="center">
+            <Grid item xs={12} md={6}>
+              <Typography>Have an account already?</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography>
+                <Link to="/login" style={{ color: "white" }}>
+                  Log in
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs = {6}>
-          <TextField
-          sx={{...hStyle}}
-          label="Last Name"
-          placeholder="Lname"
-          fullWidth
-          required
-          />
-          
-        </Grid>
-        <Grid item xs = {6}>
-          <TextField
-          sx={{...hStyle}}
-          label="SSN"
-          placeholder="Enter SSN"
-          type="number"
-          fullWidth
-          required
-          />
-        </Grid>
-        <Grid item xs = {6}>
-          <TextField
-          sx={{...hStyle}}
-          label="Address"
-          placeholder="Enter address"
-          fullWidth
-          required
-          />
-          
-        </Grid>
-        <Grid item xs={12}>
-        <TextField
-            sx={{...hStyle}}
-            label="Email"
-            placeholder="Enter email"
-            fullWidth
-            required
-          />
-        </Grid>
-        <Grid item xs={12}>
-        <TextField
-            sx={{...hStyle}}
-            label="Password"
-            placeholder="Enter password"
-            type="password"
-            fullWidth
-            required
-          />
-        </Grid>
-      </Grid>
-      <Button
-        type="submit"
-        color="primary"
-        variant="contained"
-        style={btnstyle}
-      >
-        Create Account
-      </Button>
-
-      <Grid 
-        container
-        spacing={2}
-        direction = "column"
-        alignItems = "center"
-      >
-        
-        <Grid  item xs={12} md={6}>
-        <Typography >
-        {" "}
-        Have an account already?
-        <Link href="#">Log in</Link>
-      </Typography>
-        </Grid>
-       
-      </Grid>
-    </Paper>
-  </Grid>
-);
+      </Paper>
+    </Grid>
+  );
 };
 
 export default Signup;
