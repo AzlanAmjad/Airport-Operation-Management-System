70a209e575b8816eec6e3ad1c74b71092b72c1e2
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-12 07:21:46 -0600

    fixed bug

--- backend/api/views.py
+++ backend/api/views.py
@@ -27,9 +27,11 @@ class BlacklistTokenView(APIView):
 
     def post(self, request):
         try:
-            refresh_token = request.data('refresh_token')
+            refresh_token = request.data['refresh_token']
+            print(refresh_token)
             token = RefreshToken(refresh_token)
             token.blacklist()
+            return Response(status=status.HTTP_200_OK)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
