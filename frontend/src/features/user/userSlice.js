fa69c1226fa7b7a8fb5782c969794e62e556633d
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-11 23:55:41 -0600

    login redux action

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -1,16 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit";
-const user = localStorage.getItem("access_token");
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../../components/Axios";
 
-const initialState = user
-  ? { isLoggedIn: true, user }
-  : { isLoggedIn: false, user: null };
+const access = localStorage.getItem("access_token");
+
+const initialState = access
+  ? { isLoggedIn: true}
+  : { isLoggedIn: false};
+
+// async signup action
+export const signup = createAsyncThunk("user/signup", async () => {});
+
+// async login action
+export const login = createAsyncThunk(
+  "user/login",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const result = await axiosInstance.post("token/", {
+        email: email,
+        password: password,
+      });
+      // set jwt in local storage
+      localStorage.setItem("access_token", result.data.access);
+      localStorage.setItem("refresh_token", result.data.refresh);
+      axiosInstance.defaults.headers["Authorization"] =
+        "JWT " + localStorage.getItem("access_token");
+      console.log(result.data);
+      return result;
+    } catch (err) {
+      console.log(err);
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
 
 // user slice of state
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {},
+  reducers: {
+    logout(state, action) {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("access_token");
+      state.isLoggedIn = false;
+    },
+  },
+  extraReducers: {
+    [login.fulfilled]: (state, action) => {
+      state.isLoggedIn = true;
+    },
+    [login.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+    },
+  },
 });
 
 // actions
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,10 +3,12 @@ import PersonIcon from "@mui/icons-material/Person";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import axiosInstance from "../components/Axios";
+import { useDispatch } from "react-redux";
+import { login } from "../features/user/userSlice";
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const hStyle = {
     svg: "white",
@@ -46,24 +48,8 @@ const Login = () => {
 
   // on click log in
   const log_in = async () => {
-    try {
-      const result = await axiosInstance.post("token/", {
-        email: email,
-        password: password,
-      });
-
-      // set jwt in local storage
-      localStorage.setItem("access_token", result.data.access);
-      localStorage.setItem("refresh_token", result.data.refresh);
-      axiosInstance.defaults.headers["Authorization"] =
-        "JWT " + localStorage.getItem("access_token");
-
-      console.log(result.data)
-
-      navigate("/");
-    } catch (err) {
-      console.log(err);
-    }
+    dispatch(login({email, password}))
+    navigate("/")
   };
 
   return (
