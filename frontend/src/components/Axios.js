949295a37caa0a6a070488dbf00b3dde27abb4f8
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-12 01:13:51 -0600

    Update Axios.js

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -35,7 +35,7 @@ axiosInstance.interceptors.response.use(
       error.response.status === 401 &&
       originalRequest.url === baseURL + "token/refresh/"
     ) {
-      window.location.href = "/login/";
+      window.location.href = "/login";
       return Promise.reject(error);
     }
 
