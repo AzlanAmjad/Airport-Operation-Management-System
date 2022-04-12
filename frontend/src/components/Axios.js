9ee4c00e2db8327d4aa4239ea224b80e80ec7197
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-12 04:12:45 -0600

    im too good at this i swear :smirk_cat:

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   timeout: 5000,
   headers: {
     Authorization: localStorage.getItem("access_token")
-      ? "JWT " + localStorage.getItem("access_token")
+      ? localStorage.getItem("access_token")
       : null,
     "Content-Type": "application/json",
     accept: "application/json",
--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -3,7 +3,21 @@ import axiosInstance from "../../components/Axios";
 
 const access = localStorage.getItem("access_token");
 
-const initialState = access ? { isLoggedIn: true } : { isLoggedIn: false };
+const initialState = access
+  ? {
+      isLoggedIn: true,
+      email: localStorage.getItem("email"),
+      passenger: localStorage.getItem("passenger") === "true",
+      airport_admin: localStorage.getItem("airport_admin") === "true",
+      airline_admin: localStorage.getItem("airline_admin") === "true",
+    }
+  : {
+      isLoggedIn: false,
+      email: null,
+      passenger: null,
+      airport_admin: null,
+      airline_admin: null,
+    };
 
 // async login action
 export const login = createAsyncThunk(
@@ -18,11 +32,18 @@ export const login = createAsyncThunk(
       localStorage.setItem("access_token", result.data.access);
       localStorage.setItem("refresh_token", result.data.refresh);
       axiosInstance.defaults.headers["Authorization"] =
-        "JWT " + localStorage.getItem("access_token");
+        localStorage.getItem("access_token");
 
       console.log(result.data);
 
-      return result.data;
+      // get the user
+      const user = await axiosInstance.get("user/");
+      localStorage.setItem("email", user.data.email);
+      localStorage.setItem("passenger", user.data.a_passenger);
+      localStorage.setItem("airport_admin", user.data.an_airport_admin);
+      localStorage.setItem("airline_admin", user.data.an_airline_admin);
+
+      return user.data;
     } catch (err) {
       console.log(err);
       return thunkAPI.rejectWithValue();
@@ -37,6 +58,10 @@ export const logout = createAsyncThunk("user/logout", async () => {
   });
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
+  localStorage.removeItem("email");
+  localStorage.removeItem("passenger");
+  localStorage.removeItem("airport_admin");
+  localStorage.removeItem("airline_admin");
   axiosInstance.defaults.headers["Authorization"] = null;
 });
 
@@ -48,13 +73,25 @@ export const userSlice = createSlice({
   extraReducers: {
     [login.fulfilled]: (state, action) => {
       state.isLoggedIn = true;
+      state.email = action.payload.email
+      state.passenger = action.payload.a_passenger
+      state.airport_admin = action.payload.an_airport_admin
+      state.airline_admin = action.payload.an_airline_admin
     },
     [login.rejected]: (state, action) => {
       state.isLoggedIn = false;
+      state.email = null
+      state.passenger = null
+      state.airport_admin = null
+      state.airline_admin = null
     },
     [logout.fulfilled]: (state, action) => {
       state.isLoggedIn = false;
-    }
+      state.email = null
+      state.passenger = null
+      state.airport_admin = null
+      state.airline_admin = null
+    },
   },
 });
 
