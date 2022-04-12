95297a9d8f6601ec3d265a53c1d2d3a9d7709aeb
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-12 14:02:43 -0600

    Update cartSlice.js

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  value: {},
-};
+const initialState = { quantity: 0, items: [] };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {},
+  reducers: {
+    add(state, action) {
+      state.items.push(action.payload);
+      state.quantity += 1;
+    },
+    remove(state, action) {
+      if (state.quantity != 0) {
+        const index = state.items.findIndex((object) => {
+          return object.id === action.payload.id;
+        });
+        if (index != -1) {
+          state.items.splice(index, 1);
+          state.quantity -= 1;
+        }
+      }
+    },
+  },
 });
 
 // actions
