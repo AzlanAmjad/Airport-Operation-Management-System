c923e505944f7662bd825a5d363d5952c22a8e1e
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-13 07:32:29 -0600

    Update MyComplaints.js

--- frontend/src/pages/MyComplaints.js
+++ frontend/src/pages/MyComplaints.js
@@ -1,89 +1,44 @@
 import * as React from "react";
 import { useState } from "react";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button'
-
-
-
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { ClipLoader } from "react-spinners";
 
 const MyComplaints = () => {
+  const [loading, setLoading] = useState(false);
+  const [airportComplaints, setAirportComplaints] = useState([]);
+  const [airlineComplaints, setAirlineComplaints] = useState([]);
 
-    const [user, setUser] = useState("Passenger1");
-    const [complaints, setComplaints] = useState([
-
-        { id: 0, against: 'YYC International Airport', description: 'This place trash', resolved: 'email' },
-        { id: 0, against: 'YYC International Airport', description: 'This place trash', resolved: 'email' },
-        { id: 0, against: 'YYC International Airport', description: 'This place trash', resolved: null },
-        { id: 0, against: 'YYC International Airport', description: 'This place trash', resolved: null },
-        { id: 0, against: 'YYC International Airport', description: 'This place trash', resolved: null },
-
-    ]);
-
-
-
-
-    return (
-
-        <Grid
-            container
-            justifyContent="center"
-            spacing={5}
-
-        >
-            <Grid item xs={12}>
-                <Typography variant="h2" component="div" gutterBottom>
-                    {user}'s Complaints
-                </Typography>
-            </Grid>
-
-            <Grid item>
-
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Complaint ID</TableCell>
-                                <TableCell align="left">Against</TableCell>
-                                <TableCell align="left">Description</TableCell>
-                                <TableCell align="left">Status</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {complaints.map((complaint) => (
-                                <TableRow
-                                    key={complaint.id}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell >
-                                        {complaint.id}
-                                    </TableCell>
-                                    <TableCell align="left">{complaint.against}</TableCell>
-                                    <TableCell align="left">{complaint.description}</TableCell>
-                                    <TableCell align="left">{!complaint.resolved ? (<Typography variant="h8" color="red">Not Resolved</Typography>)
-                                        : (<Typography variant="h8" color="green">Resolved</Typography>)}</TableCell>
-
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Grid>
-
-
+  return (
+    <>
+      {loading ? (
+        <Grid item>
+          <ClipLoader loading={loading} size={70} color={"#ffffff"} />
         </Grid>
-
-
-
-    );
-}
+      ) : (
+        <Grid
+          container
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          rowSpacing={4}
+        >
+          <Grid item>
+            <Typography variant="h5" fontWeight="bold">
+              Your complaints
+            </Typography>
+          </Grid>
+        </Grid>
+      )}
+    </>
+  );
+};
 
 export default MyComplaints;
