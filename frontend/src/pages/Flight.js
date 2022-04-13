8b882145018870d6a636ff36819dae177155543b
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-13 00:22:11 -0600

    react spinners

--- frontend/src/pages/Flight.js
+++ frontend/src/pages/Flight.js
@@ -1,153 +1,144 @@
 import { Grid, Typography, Paper, IconButton } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { ClipLoader } from "react-spinners";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import axiosInstance from "../components/Axios";
 
 const Flight = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
 
+  // state letting us know we are still loading data
+  const [loading, setLoading] = useState(true);
+
   // fetch flight from API
-  const [flight, setFlight] = useState({
-    flight_num: 3,
-    airline_name: "WestJet",
-    dep_time: "1997-12-17 07:37:16-08",
-    arrival_time: "1997-12-17 07:37:16-08",
-    dest_code: "YYZ",
-    plane_id: 0,
-  });
+  const [flight, setFlight] = useState({});
 
   // fetch fares for this flight from API
-  const [fares, setFare] = useState([
-    {
-      fare_id: 1,
-      price: "600",
-      cabin: "First",
-      tickets: 150,
-    },
-    {
-      fare_id: 2,
-      price: "500",
-      cabin: "Business",
-      tickets: 200,
-    },
-    {
-      fare_id: 3,
-      price: "400",
-      cabin: "Premium Economy",
-      tickets: 300,
-    },
-    {
-      fare_id: 4,
-      price: "300",
-      cabin: "Economy",
-      tickets: 600,
-    },
-  ]);
+  const [fares, setFare] = useState([]);
 
   useEffect(async () => {
     try {
       // get flight
-      
+      const flight = await axiosInstance.get(`flight/${id}`);
+      console.log(flight.data);
+      setFlight(flight.data);
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
 
+    try {
+      // get fares
+      const fares = await axiosInstance.get(`fares/${id}`);
+      console.log(fares.data);
+      setFare(fares.data);
+    } catch (err) {
+      console.log(err);
     }
-  }, [id])
+
+    setLoading(false);
+  }, [id]);
 
   return (
-    <Grid
-      item
-      container
-      direction="column"
-      alignItems="flex-start"
-      justifyContent="flex-start"
-      wrap="nowrap"
-      width="70%"
-      paddingY="50px"
-      rowSpacing={4}
-    >
-      <Grid
-        item
-        container
-        direction="column"
-        alignItems="flex-start"
-        justifyContent="flex-start"
-        rowSpacing={1}
-      >
+    <>
+      {loading ? (
         <Grid item>
-          <Typography variant="h4">Calgary to </Typography>
+          <ClipLoader loading={loading} size={50} />
         </Grid>
-        <Grid item>
-          <Typography>
-            {flight.airline_name} -{" "}
-            {flight.dep_time.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Typography>
-          {flight.dep_time.match(/[0-9]{2}:[0-9]{2}/)} -{" "}
-          {flight.arrival_time.match(/[0-9]{2}:[0-9]{2}/)}
-        </Typography>
-      </Grid>
-      <Grid
-        item
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="space-evenly"
-        rowSpacing={3}
-        my="50px"
-      >
-        <Grid item>
-          <Typography variant="h6">
-            Select Fare to 
-          </Typography>
-        </Grid>
-
-        {fares.map((fare) => {
-          return (
-            <Grid item key={`${fare.fare_id}`} sx={{ width: "inherit" }}>
-              <Paper elevation={12} sx={{ padding: "30px" }}>
-                <Grid
-                  container
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  wrap="nowrap"
-                >
-                  <Grid
-                    item
-                    container
-                    direction="column"
-                    alignItems="flex-start"
-                    justifyContent="space-evenly"
-                    rowSpacing={1}
-                  >
-                    <Grid item variant="h5" fontWeight="bold">
-                      CA ${fare.price}
-                    </Grid>
-                    <Grid item>
-                      <Typography>{fare.cabin} Class</Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography>{fare.tickets} Tickets Left</Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item mr={2}>
-                    <IconButton color="inherit" onClick={() => {}}>
-                      <AddShoppingCartIcon fontSize="large" />
-                    </IconButton>
-                  </Grid>
-                </Grid>
-              </Paper>
+      ) : (
+        <Grid
+          item
+          container
+          direction="column"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+          wrap="nowrap"
+          width="70%"
+          paddingY="50px"
+          rowSpacing={4}
+        >
+          <Grid
+            item
+            container
+            direction="column"
+            alignItems="flex-start"
+            justifyContent="flex-start"
+            rowSpacing={1}
+          >
+            <Grid item>
+              <Typography variant="h4">Calgary to </Typography>
             </Grid>
-          );
-        })}
-      </Grid>
-    </Grid>
+            <Grid item>
+              <Typography>
+                {flight.airline_name} -{" "}
+                {flight.dep_time.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography>
+              {flight.dep_time.match(/[0-9]{2}:[0-9]{2}/)} -{" "}
+              {flight.arrival_time.match(/[0-9]{2}:[0-9]{2}/)}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            alignItems="center"
+            justifyContent="space-evenly"
+            rowSpacing={3}
+            my="50px"
+          >
+            <Grid item>
+              <Typography variant="h6">Select Fare to</Typography>
+            </Grid>
+
+            {fares.map((fare) => {
+              return (
+                <Grid item key={`${fare.fare_id}`} sx={{ width: "inherit" }}>
+                  <Paper elevation={12} sx={{ padding: "30px" }}>
+                    <Grid
+                      container
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-between"
+                      wrap="nowrap"
+                    >
+                      <Grid
+                        item
+                        container
+                        direction="column"
+                        alignItems="flex-start"
+                        justifyContent="space-evenly"
+                        rowSpacing={1}
+                      >
+                        <Grid item variant="h5" fontWeight="bold">
+                          CA ${fare.price}
+                        </Grid>
+                        <Grid item>
+                          <Typography>{fare.cabin} Class</Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography>{fare.tickets} Tickets Left</Typography>
+                        </Grid>
+                      </Grid>
+                      <Grid item mr={2}>
+                        <IconButton color="inherit" onClick={() => {}}>
+                          <AddShoppingCartIcon fontSize="large" />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 };
 
