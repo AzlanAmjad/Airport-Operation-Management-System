9542aef6e17c101a98e6e0457a8103b964f7666c
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-13 07:44:09 -0600

    Update MyComplaints.js

--- frontend/src/pages/MyComplaints.js
+++ frontend/src/pages/MyComplaints.js
@@ -28,12 +28,52 @@ const MyComplaints = () => {
           direction="column"
           justifyContent="flex-start"
           alignItems="flex-start"
-          rowSpacing={4}
+          rowSpacing={5}
         >
           <Grid item>
             <Typography variant="h5" fontWeight="bold">
               Your complaints
             </Typography>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            alignItems="flex-start"
+            rowSpacing={2}
+          >
+            <Grid item>
+              <Typography variant="h6">Airport Complaints</Typography>
+            </Grid>
+            {airportComplaints.length === 0 ? (
+              <Grid item ml="20px">
+                <Typography>No airport complaints</Typography>
+              </Grid>
+            ) : (
+              <Grid item container ml="20px">
+                map complaints here
+              </Grid>
+            )}
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            alignItems="flex-start"
+            rowSpacing={2}
+          >
+            <Grid item>
+              <Typography variant="h6">Airline Complaints</Typography>
+            </Grid>
+            {airlineComplaints.length === 0 ? (
+              <Grid item ml="20px">
+                <Typography>No airline complaints</Typography>
+              </Grid>
+            ) : (
+              <Grid item container ml="20px">
+                map complaints here
+              </Grid>
+            )}
           </Grid>
         </Grid>
       )}
