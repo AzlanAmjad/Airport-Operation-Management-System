5515d9399be6b6dd7a45f7bb761fd48329fa5dfd
Tr1d3nt <saudagha2@gmail.com>
2022-04-13 14:20:09 -0600

    remove

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,125 +1,103 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button, Divider, Typography } from "@mui/material";
-import { Grid } from "@mui/material";
+
 import { Link } from "react-router-dom";
-
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { Grid, Paper, IconButton } from "@mui/material";
+import { remove } from "../features/cart/cartSlice";
 
 const Cart = () => {
-  const [cartt, setCartt] = useState([]);
-  ;
-  const [page, setPage] = useState('cartt');
 
-  const [products, setProducts] = useState([
-    {
-      name: 'YYC-YYZ',
-      cost: 32.99
-    },
-    {
-      name: 'YYC-YOW',
-      cost: 52.99
-    }
-    ,
-    {
-      name: 'YYC-YUL',
-      cost: 25.49
-    }
-  ])
+  const dispatch = useDispatch();
 
-  const addToCart = (product) => {
+  const { items } = useSelector((state) => state.cart);
 
-    setCartt([...cartt, { ...product }]);
-  };
-
-  const totalCost = () => {
-
-    return products.reduce((num, { cost }) => num + cost, 0)
-
-  }
-
-  const removeFromCart = (productRemoval) => {
-    setProducts(
-      products.filter((product) => product !== productRemoval))
-  };
-
-  const navigateTo = (newPage) => {
-    setPage(newPage);
-  }
-
-  const renderProducts = () => (
-    <div>
-      <h1>Fares</h1>
-      <div className="products">
-        {products.map((product, idx) => (
-          <div className="product" key={idx}>
-            <h3>{product.name}</h3>
-            <Typography>${product.cost}</Typography>
-            <Button variant="contained" onClick={() => addToCart(product)}>Add to Cart</Button>
-            <Button variant="contained" onClick={() => removeFromCart(product)}>remove</Button>
-          </div>
-
-        ))}
-      </div>
-    </div>
-  )
-
-  const renderCart = () => (
-    <div>
-      <h1> Your Cart</h1>
-      <div className="products">
-        {products.map((product, idx) => (
-          <div className="product" key={idx}>
-            <Grid
-              item
-              container spacing={7}
-              justifyContent="center">
-              <Grid item >
-                <h3>Flight :{product.name}</h3>
-
-              </Grid>
-              <Grid item>
-                <h4>Price: $ {product.cost}</h4>
-
-              </Grid>
-              <Divider />
-
-              <Grid item>
-                <Button variant="contained" sx={{ marginTop: 2.2 }} onClick={() => removeFromCart(product)}>Remove</Button>
-              </Grid>
-            </Grid>
-          </div>
-
-        ))}
-        <Divider></Divider>
-      </div>
-      <Grid
-        item
-        container spacing={12}
-        justifyContent="flex-start">
-        <Grid item xs={5}>
-          <h4>Total Cost: </h4>
-        </Grid>
-        <Grid item xs={1}>
-          <h4>${totalCost()} </h4>
-        </Grid>
-        <Grid item xs={12}>
-          <Link style={{ textDecoration: 'none' }} to="/checkout">
-            <Button sx variant="contained"> Proceed to Checkout </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </div>
-  )
 
   return (
-    <div >
 
-      {page === 'products' && renderProducts()}
-      {page === 'cartt' && renderCart()}
+    <Grid
+      container
+      justifyContent="center"
+
+    >
+
+      <Grid item container direction="column" spacing={2} xs={6}>
+
+        {items.map((item) => (
+
+          <Grid
+            item
+            sx={{ width: "100%" }}
+          >
+            <Paper elevation={12} sx={{ padding: "30px" }}>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                wrap="nowrap"
+              >
+
+                <Grid
+                  item
+                  container
+                  direction="column"
+                  alignItems="flex-start"
+                  justifyContent="space-evenly"
+                  rowSpacing={1}
+                >
+                  {!!item.name ? (
+                    <Grid item >
+                      <Typography variant="h2">{item.name}</Typography>
+                    </Grid>
+                  ) : (
+                    <Grid item >
+                      <Typography variant="h2">{item.cabin}</Typography>
+                    </Grid>
+                  )}
+                  <Grid item>
+                    {
+                      <Typography variant="h7">Price :{item.price}</Typography>
+
+                    }
+
+                  </Grid>
+
+                </Grid>
+                {item.type === "stay" ? (
+                  <Grid item >
+                    <Button variant="contained" sx={{ marginTop: 2.2 }} onClick={() => {
+                      dispatch(remove({ ...item, type: "stay" }));
+                    }}>Remove</Button>
+                  </Grid>
+                ) : (
+
+                  <Grid item >
+                    <Button variant="contained" sx={{ marginTop: 2.2 }} onClick={() => {
+                      dispatch(remove({ ...item, type: "fare" }));
+                    }}>Remove</Button>
+                  </Grid>
+                )}
+              </Grid>
+            </Paper>
+          </Grid>
+        ))}
+        <Grid item>
+          <Divider></Divider>
+        </Grid>
 
 
-    </div>
+
+
+
+      </Grid>
+
+
+
+    </Grid>
+
+
+
   );
 }
 export default Cart
