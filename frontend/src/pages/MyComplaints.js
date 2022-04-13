d5915b161cd67d0dc9b756b32916013364e140bb
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-13 07:45:53 -0600

    Update MyComplaints.js

--- frontend/src/pages/MyComplaints.js
+++ frontend/src/pages/MyComplaints.js
@@ -2,12 +2,6 @@ import * as React from "react";
 import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { ClipLoader } from "react-spinners";
 
