409561dc1d17b3856cd1df4767881abcbe116fbf
AzlanAmjad <Azlanamjad_123@hotmail.com>
2022-04-15 03:39:00 -0600

    Update Cart.js

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -15,122 +15,132 @@ const Cart = () => {
 
   useEffect(async () => {
     try {
-      const passenger = await axiosInstance.get(`passenger/${id}/`)
-      console.log(passenger.data)
-      setPassenger(passenger.data)
+      const passenger = await axiosInstance.get(`passenger/${id}/`);
+      console.log(passenger.data);
+      setPassenger(passenger.data);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }, [id])
+  }, [id]);
 
   const handleCheckout = async () => {
-    const books = []
+    const books = [];
     items.map((item) => {
       if (item.type === "stay") {
 
       }
-    })
+    });
   };
 
   return (
-    <Grid container justifyContent="center">
-      <Grid item container direction="column" spacing={2} xs={6}>
-        {items.map((item) => (
-          <Grid item sx={{ width: "100%" }}>
-            <Paper elevation={12} sx={{ padding: "30px" }}>
-              <Grid
-                container
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-                wrap="nowrap"
-              >
-                <Grid
-                  item
-                  container
-                  direction="column"
-                  alignItems="flex-start"
-                  justifyContent="space-evenly"
-                  rowSpacing={1}
-                >
-                  {!!item.name ? (
-                    <Grid item>
-                      <Typography variant="h2">{item.name}</Typography>
-                    </Grid>
-                  ) : (
-                    <Grid item>
-                      <Typography variant="h2">{item.cabin}</Typography>
-                    </Grid>
-                  )}
-                  <Grid item>
-                    {<Typography variant="h7">Price :{item.price}</Typography>}
-                  </Grid>
-                </Grid>
-                {item.type === "stay" ? (
+    <>
+      {items.length === 0 ? (
+        <Typography variant="h2">Your cart is empty</Typography>
+      ) : (
+        <Grid item container justifyContent="center">
+          <Grid item container direction="column" spacing={2} xs={6}>
+            {items.map((item) => (
+              <Grid item sx={{ width: "100%" }}>
+                <Paper elevation={12} sx={{ padding: "30px" }}>
                   <Grid
-                    item
                     container
-                    justifyContent="space-evenly"
-                    direction="column"
-                    alignItems="flex-end"
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    wrap="nowrap"
                   >
-                    <Grid item>
-                      <Button
-                        variant="contained"
-                        sx={{ marginTop: 2.2 }}
-                        onClick={() => {
-                          dispatch(remove({ ...item, type: "stay" }));
-                        }}
-                      >
-                        Remove
-                      </Button>
-                    </Grid>
-                  </Grid>
-                ) : (
-                  <Grid item>
-                    <Button
-                      variant="contained"
-                      sx={{ marginTop: 2.2 }}
-                      onClick={() => {
-                        dispatch(remove({ ...item, type: "fare" }));
-                      }}
+                    <Grid
+                      item
+                      container
+                      direction="column"
+                      alignItems="flex-start"
+                      justifyContent="space-evenly"
+                      rowSpacing={1}
                     >
-                      Remove
-                    </Button>
+                      {!!item.name ? (
+                        <Grid item>
+                          <Typography variant="h2">{item.name}</Typography>
+                        </Grid>
+                      ) : (
+                        <Grid item>
+                          <Typography variant="h2">{item.cabin}</Typography>
+                        </Grid>
+                      )}
+                      <Grid item>
+                        {
+                          <Typography>
+                            CA ${item.price}
+                          </Typography>
+                        }
+                      </Grid>
+                    </Grid>
+                    {item.type === "stay" ? (
+                      <Grid
+                        item
+                        container
+                        justifyContent="space-evenly"
+                        direction="column"
+                        alignItems="flex-end"
+                      >
+                        <Grid item>
+                          <Button
+                            variant="contained"
+                            sx={{ marginTop: 2.2 }}
+                            onClick={() => {
+                              dispatch(remove({ ...item, type: "stay" }));
+                            }}
+                          >
+                            Remove
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    ) : (
+                      <Grid item>
+                        <Button
+                          variant="contained"
+                          sx={{ marginTop: 2.2 }}
+                          onClick={() => {
+                            dispatch(remove({ ...item, type: "fare" }));
+                          }}
+                        >
+                          Remove
+                        </Button>
+                      </Grid>
+                    )}
                   </Grid>
-                )}
+                </Paper>
               </Grid>
-            </Paper>
-          </Grid>
-        ))}
-        <Grid item>
-          <Divider></Divider>
-        </Grid>
+            ))}
+            <Grid item>
+              <Divider></Divider>
+            </Grid>
 
-        <Grid
-          item
-          container
-          direction="column"
-          alignItems="flex-end"
-          justifyContent="space-evenly"
-        >
-          <Grid item>
-            <Typography variant="h2">Total: ${total}.00</Typography>
-          </Grid>
-          <Grid item>
-            <Button
-              variant="contained"
-              sx={{ marginTop: 2.2 }}
-              onClick={() => {
-                handleCheckout();
-              }}
+            <Grid
+              item
+              container
+              direction="column"
+              alignItems="flex-end"
+              justifyContent="space-evenly"
             >
-              Checkout
-            </Button>
+              <Grid item>
+                <Typography variant="h2">Total: ${total}.00</Typography>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  sx={{ marginTop: 2.2 }}
+                  onClick={() => {
+                    handleCheckout();
+                  }}
+                >
+                  Checkout
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
+      )}
+    </>
   );
 };
 export default Cart;
